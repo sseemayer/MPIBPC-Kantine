16b446f7f04c4ebf887a0b934f7eedefcf9f4703
Stefan Seemayer <stefan@seemayer.de>
2015-06-23 11:22:24 +0200

    Skip problematic PDFs

--- download_menus.py
+++ download_menus.py
@@ -21,22 +21,27 @@ def main():
     dl_urls = [mu for mu in menu_urls if not r.hget("seen", mu)]
 
     for du in dl_urls:
-        print("download: {0}".format(du))
 
-        with tempfile.NamedTemporaryFile() as f:
-            kantine.download.download_menu(du, f)
-            menu = kantine.parse.parse_menu(f.name)
+        try:
+            print("download: {0}".format(du))
 
-        meals = []
-        for mtype in menu.columns[1:]:
-            for date, meal in zip(menu['date'], menu[mtype]):
-                if meal.strip():
-                    meals.append(kantine.models.Meal(name=meal, name_en=None, date=date, mealtype=mtype))
+            with tempfile.NamedTemporaryFile() as f:
+                kantine.download.download_menu(du, f)
+                menu = kantine.parse.parse_menu(f.name)
 
-        kantine.database.session.add_all(meals)
-        kantine.database.session.commit()
+            meals = []
+            for mtype in menu.columns[1:]:
+                for date, meal in zip(menu['date'], menu[mtype]):
+                    if meal.strip():
+                        meals.append(kantine.models.Meal(name=meal, name_en=None, date=date, mealtype=mtype))
 
-        r.hset("seen", du, datetime.datetime.now())
+            kantine.database.session.add_all(meals)
+            kantine.database.session.commit()
+
+            r.hset("seen", du, datetime.datetime.now())
+
+        except Exception as e:
+            print("Error: {0}".format(e))
 
 if __name__ == '__main__':
     main()
